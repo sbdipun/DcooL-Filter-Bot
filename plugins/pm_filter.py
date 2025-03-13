a436db0a66e89eabce961ecfc66b7c0656366ca4
Nandhakumar K <116391685+Tamilupdates@users.noreply.github.com>
2025-03-13 11:53:59 +0530

    new

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1437,7 +1437,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     
     elif query.data.startswith("checksub"):
         if UPDATES_CHANNEL and not await is_subscribed(client, query):
-            await query.answer("Jᴏɪɴ ᴏᴜʀ Bᴀᴄᴋ-ᴜᴘ ᴄʜᴀɴɴᴇʟ ᴍᴀʜɴ! 😒", show_alert=True)
+            await query.answer("Jᴏɪɴ ᴏᴜʀ Uᴘᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ ᴍᴀʜɴ! 😒", show_alert=True)
             return
         ident, kk, file_id = query.data.split("#")
         await query.answer(url=f"https://t.me/{temp.U_NAME}?start={kk}_{file_id}")
